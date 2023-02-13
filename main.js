f12d30456762a64a1bbf4d94d2a5d2430ba3abe5
henry mutua <mutuahenry116@gmail.com>
2023-02-13 20:50:41 +0300

    Chose flex over grid

--- main.js
+++ main.js
@@ -5,26 +5,49 @@ container.setAttribute('class', 'container');
 body.appendChild(container);
 
 const div1 = document.createElement('div');
-div1.setAttribute('id', 'one');
+div1.setAttribute('class', 'grid-square');
 container.appendChild(div1);
 
 const div2 = document.createElement('div');
-div2.setAttribute('id', 'two');
+div2.setAttribute('class', 'grid-square');
 container.appendChild(div2);
 
 const div3 = document.createElement('div');
-div3.setAttribute('id', 'three');
+div3.setAttribute('class', 'grid-square');
 container.appendChild(div3);
 
 const div4 = document.createElement('div');
-div4.setAttribute('id', 'four');
+div4.setAttribute('class', 'grid-square');
 container.appendChild(div4);
 
 const div5 = document.createElement('div');
-div5.setAttribute('id', 'five');
+div5.setAttribute('class', 'grid-square');
 container.appendChild(div5);
 
 const div6 = document.createElement('div');
-div6.setAttribute('id', 'six');
+div6.setAttribute('class', 'grid-square');
 container.appendChild(div6);
 
+const div7 = document.createElement('div');
+div7.setAttribute('class', 'grid-square');
+container.appendChild(div7);
+
+const div8 = document.createElement('div');
+div8.setAttribute('class', 'grid-square');
+container.appendChild(div8);
+
+const div9 = document.createElement('div');
+div9.setAttribute('class', 'grid-square');
+container.appendChild(div9);
+
+const div10 = document.createElement('div');
+div10.setAttribute('class', 'grid-square');
+container.appendChild(div10);
+
+const div11 = document.createElement('div');
+div11.setAttribute('class', 'grid-square');
+container.appendChild(div11);
+
+const div12 = document.createElement('div');
+div12.setAttribute('class', 'grid-square');
+container.appendChild(div12);
