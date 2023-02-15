96caa60ae266a00a7f2f3104943380d1f27ab0a3
henry mutua <mutuahenry116@gmail.com>
2023-02-15 22:10:46 +0300

    grid created

--- main.js
+++ main.js
@@ -51,3 +51,315 @@ container.appendChild(div11);
 const div12 = document.createElement('div');
 div12.setAttribute('class', 'grid-square');
 container.appendChild(div12);
+
+const div13 = document.createElement('div');
+div13.setAttribute('class', 'grid-square');
+container.appendChild(div13);
+
+const div14 = document.createElement('div');
+div14.setAttribute('class', 'grid-square');
+container.appendChild(div14);
+
+const div15 = document.createElement('div');
+div15.setAttribute('class', 'grid-square');
+container.appendChild(div15);
+
+const div16 = document.createElement('div');
+div16.setAttribute('class', 'grid-square');
+container.appendChild(div16);
+
+const div17 = document.createElement('div');
+div17.setAttribute('class', 'grid-square');
+container.appendChild(div17);
+
+const div18 = document.createElement('div');
+div18.setAttribute('class', 'grid-square');
+container.appendChild(div18);
+
+const div19 = document.createElement('div');
+div19.setAttribute('class', 'grid-square');
+container.appendChild(div19);
+
+const div20 = document.createElement('div');
+div20.setAttribute('class', 'grid-square');
+container.appendChild(div20);
+
+const div21 = document.createElement('div');
+div21.setAttribute('class', 'grid-square');
+container.appendChild(div21);
+
+const div22 = document.createElement('div');
+div22.setAttribute('class', 'grid-square');
+container.appendChild(div22);
+
+const div23 = document.createElement('div');
+div23.setAttribute('class', 'grid-square');
+container.appendChild(div23);
+
+const div24 = document.createElement('div');
+div24.setAttribute('class', 'grid-square');
+container.appendChild(div24);
+
+const div25 = document.createElement('div');
+div25.setAttribute('class', 'grid-square');
+container.appendChild(div25);
+
+const div26 = document.createElement('div');
+div26.setAttribute('class', 'grid-square');
+container.appendChild(div26);
+
+const div27 = document.createElement('div');
+div27.setAttribute('class', 'grid-square');
+container.appendChild(div27);
+
+const div28 = document.createElement('div');
+div28.setAttribute('class', 'grid-square');
+container.appendChild(div28);
+
+const div29 = document.createElement('div');
+div29.setAttribute('class', 'grid-square');
+container.appendChild(div29);
+
+const div30 = document.createElement('div');
+div30.setAttribute('class', 'grid-square');
+container.appendChild(div30);
+
+const div31 = document.createElement('div');
+div31.setAttribute('class', 'grid-square');
+container.appendChild(div31);
+
+const div32 = document.createElement('div');
+div32.setAttribute('class', 'grid-square');
+container.appendChild(div32);
+
+const div33 = document.createElement('div');
+div33.setAttribute('class', 'grid-square');
+container.appendChild(div33);
+
+const div34 = document.createElement('div');
+div34.setAttribute('class', 'grid-square');
+container.appendChild(div34);
+
+const div35 = document.createElement('div');
+div35.setAttribute('class', 'grid-square');
+container.appendChild(div35);
+
+const div36 = document.createElement('div');
+div36.setAttribute('class', 'grid-square');
+container.appendChild(div36);
+
+const div37 = document.createElement('div');
+div37.setAttribute('class', 'grid-square');
+container.appendChild(div37);
+
+const div38 = document.createElement('div');
+div38.setAttribute('class', 'grid-square');
+container.appendChild(div38);
+
+const div39 = document.createElement('div');
+div39.setAttribute('class', 'grid-square');
+container.appendChild(div39);
+
+const div40 = document.createElement('div');
+div40.setAttribute('class', 'grid-square');
+container.appendChild(div40);
+
+const div41 = document.createElement('div');
+div41.setAttribute('class', 'grid-square');
+container.appendChild(div41);
+
+const div42 = document.createElement('div');
+div42.setAttribute('class', 'grid-square');
+container.appendChild(div42);
+
+const div43 = document.createElement('div');
+div43.setAttribute('class', 'grid-square');
+container.appendChild(div43);
+
+const div44 = document.createElement('div');
+div44.setAttribute('class', 'grid-square');
+container.appendChild(div44);
+
+const div45 = document.createElement('div');
+div45.setAttribute('class', 'grid-square');
+container.appendChild(div45);
+
+const div46 = document.createElement('div');
+div46.setAttribute('class', 'grid-square');
+container.appendChild(div46);
+
+const div47 = document.createElement('div');
+div47.setAttribute('class', 'grid-square');
+container.appendChild(div47);
+
+const div48 = document.createElement('div');
+div48.setAttribute('class', 'grid-square');
+container.appendChild(div48);
+
+const div49 = document.createElement('div');
+div49.setAttribute('class', 'grid-square');
+container.appendChild(div49);
+
+const div50 = document.createElement('div');
+div50.setAttribute('class', 'grid-square');
+container.appendChild(div50);
+
+const div51 = document.createElement('div');
+div51.setAttribute('class', 'grid-square');
+container.appendChild(div51);
+
+const div52 = document.createElement('div');
+div52.setAttribute('class', 'grid-square');
+container.appendChild(div52);
+
+const div53 = document.createElement('div');
+div53.setAttribute('class', 'grid-square');
+container.appendChild(div53);
+
+const div54 = document.createElement('div');
+div54.setAttribute('class', 'grid-square');
+container.appendChild(div54);
+
+const div55 = document.createElement('div');
+div55.setAttribute('class', 'grid-square');
+container.appendChild(div55);
+
+const div56 = document.createElement('div');
+div56.setAttribute('class', 'grid-square');
+container.appendChild(div56);
+
+const div57 = document.createElement('div');
+div57.setAttribute('class', 'grid-square');
+container.appendChild(div57);
+
+const div58 = document.createElement('div');
+div58.setAttribute('class', 'grid-square');
+container.appendChild(div58);
+
+const div59 = document.createElement('div');
+div59.setAttribute('class', 'grid-square');
+container.appendChild(div59);
+
+const div60 = document.createElement('div');
+div60.setAttribute('class', 'grid-square');
+container.appendChild(div60);
+
+const div61 = document.createElement('div');
+div61.setAttribute('class', 'grid-square');
+container.appendChild(div61);
+
+const div62 = document.createElement('div');
+div62.setAttribute('class', 'grid-square');
+container.appendChild(div62);
+
+const div63 = document.createElement('div');
+div63.setAttribute('class', 'grid-square');
+container.appendChild(div63);
+
+const div64 = document.createElement('div');
+div64.setAttribute('class', 'grid-square');
+container.appendChild(div64);
+
+const div65 = document.createElement('div');
+div65.setAttribute('class', 'grid-square');
+container.appendChild(div65);
+
+const div66 = document.createElement('div');
+div66.setAttribute('class', 'grid-square');
+container.appendChild(div66);
+
+const div67 = document.createElement('div');
+div67.setAttribute('class', 'grid-square');
+container.appendChild(div67);
+
+const div68 = document.createElement('div');
+div68.setAttribute('class', 'grid-square');
+container.appendChild(div68);
+
+const div69 = document.createElement('div');
+div69.setAttribute('class', 'grid-square');
+container.appendChild(div69);
+
+const div70 = document.createElement('div');
+div70.setAttribute('class', 'grid-square');
+container.appendChild(div70);
+
+const div71 = document.createElement('div');
+div71.setAttribute('class', 'grid-square');
+container.appendChild(div71);
+
+const div72 = document.createElement('div');
+div72.setAttribute('class', 'grid-square');
+container.appendChild(div72);
+
+const div73 = document.createElement('div');
+div73.setAttribute('class', 'grid-square');
+container.appendChild(div73);
+
+const div74 = document.createElement('div');
+div74.setAttribute('class', 'grid-square');
+container.appendChild(div74);
+
+const div75 = document.createElement('div');
+div75.setAttribute('class', 'grid-square');
+container.appendChild(div75);
+
+const div76 = document.createElement('div');
+div76.setAttribute('class', 'grid-square');
+container.appendChild(div76);
+
+const div77 = document.createElement('div');
+div77.setAttribute('class', 'grid-square');
+container.appendChild(div77);
+
+const div78 = document.createElement('div');
+div78.setAttribute('class', 'grid-square');
+container.appendChild(div78);
+
+const div79 = document.createElement('div');
+div79.setAttribute('class', 'grid-square');
+container.appendChild(div79);
+
+const div80 = document.createElement('div');
+div80.setAttribute('class', 'grid-square');
+container.appendChild(div80);
+
+const div81 = document.createElement('div');
+div81.setAttribute('class', 'grid-square');
+container.appendChild(div81);
+
+const div82 = document.createElement('div');
+div82.setAttribute('class', 'grid-square');
+container.appendChild(div82);
+
+const div83 = document.createElement('div');
+div83.setAttribute('class', 'grid-square');
+container.appendChild(div83);
+
+const div84 = document.createElement('div');
+div84.setAttribute('class', 'grid-square');
+container.appendChild(div84);
+
+const div85 = document.createElement('div');
+div85.setAttribute('class', 'grid-square');
+container.appendChild(div85);
+
+const div86 = document.createElement('div');
+div86.setAttribute('class', 'grid-square');
+container.appendChild(div86);
+
+const div87 = document.createElement('div');
+div87.setAttribute('class', 'grid-square');
+container.appendChild(div87);
+
+const div88 = document.createElement('div');
+div88.setAttribute('class', 'grid-square');
+container.appendChild(div88);
+
+const div89 = document.createElement('div');
+div89.setAttribute('class', 'grid-square');
+container.appendChild(div89);
+
+const div90 = document.createElement('div');
+div90.setAttribute('class', 'grid-square');
+container.appendChild(div90);
